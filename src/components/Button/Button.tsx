08a0095fbbb8935c27ef9a23f0b28bed2c25492d
Ivan <ivan@inieto.dev>
2021-03-18 16:29:34 -0600

    Changed button color to linear gradiant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,14 @@
-import MaterialButton from "@material-ui/core/Button";
-import { withStyles, Theme } from "@material-ui/core/styles";
+import MaterialButton from '@material-ui/core/Button';
+import { withStyles, Theme } from '@material-ui/core/styles';
 
 const Button = withStyles((theme: Theme) => ({
   root: {
     color: theme.palette.primary.main,
-    borderColor: theme.palette.common.white,
-    "&:hover": {
-      backgroundColor: theme.palette.primary.dark,
-      color: theme.palette.common.black,
-      boxShadow: `1px 1px 5px ${theme.palette.primary.main}`,
+    borderColor: theme.palette.grey[400],
+    '&:hover': {
+      background: 'linear-gradient(to right, #141e30, #243b55)',
+      boxShadow: `1px 1px 20px #243b55`,
+      border: `1px solid #243b55`,
     },
   },
 }))(MaterialButton);
