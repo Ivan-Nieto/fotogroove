5ccac24cab694c5976f4546c3769f86d9b23f280
Ivan <inieto1@nmsu.edu>
2021-03-26 17:01:10 -0600

    Fixed re-render in spoke

---
+++ src/components/SelectionWheel/SelectionWheel.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles, useTheme, Theme } from '@material-ui/core/styles';
+
+import Spoke from './Spoke';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  select: {
+    maxWidth: '250px',
+    maxHeight: '250px',
+    height: '100%',
+    width: '100%',
+
+    padding: '10px',
+  },
+  selectText: {
+    textTransform: 'none',
+    border: 'none',
+    width: '100%',
+    fontSize: '32px',
+    fontStyle: 'oblique 20deg',
+    color: theme.palette.secondary.main,
+    textShadow: `0px 0px 30px ${theme.palette.common.white},0px 0px 30px ${theme.palette.common.white},0px 0px 10px ${theme.palette.common.white}`,
+    '&:hover': {
+      color: 'white',
+      textShadow: '0px 0px 30px white,0px 0px 30px white,0px 0px 10px white',
+    },
+  },
+  disabledText: {
+    textTransform: 'none',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '18px',
+    color: theme.palette.grey[400],
+  },
+  animationDown: {},
+}));
+
+const SelectionWheel = ({
+  options,
+  activeOption,
+  setActiveOption,
+}: {
+  options: string[];
+  activeOption: number;
+  setActiveOption: any;
+}) => {
+  const theme = useTheme();
+  const classes = useStyles(theme);
+  const getPrevious = () =>
+    activeOption - 1 < 0 ? options.length - 1 : activeOption - 1;
+
+  const getNext = () =>
+    activeOption + 1 >= options.length ? 0 : activeOption + 1;
+
+  const [prevActive, setPrevActive] = useState(activeOption);
+  const [prevOption, setPrevOption] = useState<number>(getPrevious());
+  const [nextOption, setNextOption] = useState<number>(getNext());
+  const [movingForward, setMovingForward] = useState<boolean>(true);
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    // Find out if animation should play forward or backward
+    if (activeOption === 0) {
+      setMovingForward(prevActive !== 1);
+    } else if (activeOption === options.length - 1) {
+      setMovingForward(prevActive !== 0);
+    } else setMovingForward(prevActive < activeOption);
+
+    setNextOption(getNext());
+    setPrevOption(getPrevious());
+    setAnimate(activeOption !== prevActive);
+    setPrevActive(activeOption);
+    // eslint-disable-next-line
+  }, [activeOption]);
+
+  return (
+    <div className={classes.select}>
+      <Spoke
+        animate={Boolean(animate)}
+        text={
+          prevOption == null ? options[options.length - 1] : options[prevOption]
+        }
+        direction={!movingForward ? 'top' : 'reverseTop'}
+        onClick={() => setActiveOption(getPrevious())}
+        activeOption={activeOption}
+        className={classes.disabledText}
+      />
+
+      <Spoke
+        animate={Boolean(animate)}
+        text={options[activeOption || 0]}
+        activeOption={activeOption}
+        className={classes.selectText}
+        direction={!movingForward ? 'center' : 'reverseCenter'}
+      />
+
+      <Spoke
+        animate={Boolean(animate)}
+        text={options[nextOption == null ? 1 : nextOption]}
+        activeOption={activeOption}
+        direction={!movingForward ? 'bottom' : 'reverseBottom'}
+        onClick={() => setActiveOption(getNext())}
+        className={classes.disabledText}
+      />
+    </div>
+  );
+};
+
+export default SelectionWheel;
---
+++ src/components/SelectionWheel/Spoke.tsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { useSpring, animated } from 'react-spring';
+
+interface Params {
+  text: string;
+  onClick?: () => void | undefined;
+  activeOption: number;
+  className?: string;
+  direction:
+    | 'top'
+    | 'bottom'
+    | 'center'
+    | 'reverseTop'
+    | 'reverseBottom'
+    | 'reverseCenter';
+  animate: boolean;
+}
+
+const Spoke = ({
+  text,
+  onClick,
+  activeOption,
+  className,
+  direction,
+  animate,
+}: Params) => {
+  const propsTop = useSpring({
+    reset: activeOption > -1,
+    from: {
+      opacity: 0,
+      transform: 'translate3d(0,-100%,0)',
+    },
+    to: {
+      opacity: 1,
+      transform: 'translate3d(0,0,0)',
+    },
+  });
+
+  const propsReverseTop = useSpring({
+    reset: activeOption > -1,
+    from: {
+      opacity: 1,
+      transform: 'translate3d(0,100%,0)',
+    },
+    to: {
+      opacity: 1,
+      transform: 'translate3d(0,0,0)',
+    },
+  });
+
+  const propsCenter = useSpring({
+    reset: activeOption > -1,
+    from: {
+      opacity: 1,
+      fontSize: '18',
+      transform: 'translate3d(-20%,-100%,0)',
+    },
+    to: {
+      opacity: 1,
+      fontSize: '32px',
+      transform: 'translate3d(0,0,0)',
+    },
+  });
+
+  const propsReverseCenter = useSpring({
+    reset: activeOption > -1,
+    from: {
+      opacity: 1,
+      fontSize: '18',
+      transform: 'translate3d(-20%,100%,0)',
+    },
+    to: {
+      opacity: 1,
+      fontSize: '32px',
+      transform: 'translate3d(0,0,0)',
+    },
+  });
+
+  const propsBottom = useSpring({
+    reset: activeOption > -1,
+    from: {
+      opacity: 0,
+      transform: 'translate3d(0,-100%,0)',
+    },
+    to: {
+      opacity: 1,
+      transform: 'translate3d(0,0,0)',
+    },
+  });
+
+  const propsReverseBottom = useSpring({
+    reset: activeOption > -1,
+    from: {
+      opacity: 0,
+      transform: 'translate3d(0,100%,0)',
+    },
+    to: {
+      opacity: 1,
+      transform: 'translate3d(0,0,0)',
+    },
+  });
+
+  let props = {};
+
+  switch (direction) {
+    case 'top':
+      props = propsTop;
+      break;
+    case 'bottom':
+      props = propsBottom;
+      break;
+    case 'center':
+      props = propsCenter;
+      break;
+    case 'reverseTop':
+      props = propsReverseTop;
+      break;
+    case 'reverseBottom':
+      props = propsReverseBottom;
+      break;
+    case 'reverseCenter':
+      props = propsReverseCenter;
+      break;
+  }
+
+  return (
+    <animated.div key={activeOption} style={animate ? props : {}}>
+      <Button onClick={onClick} className={className}>
+        {text}
+      </Button>
+    </animated.div>
+  );
+};
+
+export default Spoke;
