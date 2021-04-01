bd69125fd47219d66af58ea3a2be7b74ad5df623
Ivan <inieto1@nmsu.edu>
2021-04-01 16:35:39 -0600

    Fixed search bar not updating when typing and changed scroll bar color in search bar drop down

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import {
+  makeStyles,
+  withStyles,
+  useTheme,
+  Theme,
+} from '@material-ui/core/styles';
+import MuPaper from '@material-ui/core/Paper';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import Input from '../Input/Input';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '280px',
+  },
+}));
+
+const Paper = withStyles((theme: Theme) => ({
+  root: {
+    backgroundColor: 'rgba(37, 40, 43, 0.8)',
+    borderRadius: '10px',
+    backdropFilter: 'blur(20px)',
+    color: theme.palette.common.white,
+
+    '& ::-webkit-scrollbar': {
+      width: '10px',
+    },
+
+    '& ::-webkit-scrollbar-track': {
+      backgroundColor: '#414345',
+    },
+
+    '& ::-webkit-scrollbar-thumb': {
+      background: 'linear-gradient(to right, #141e30, #243b55)',
+      outline: '1px solid #414345',
+    },
+  },
+}))(MuPaper);
 
 interface Props {
   label: string;
@@ -12,14 +47,17 @@ interface Props {
 }
 
 const Search = (props: Props) => {
+  useTheme();
+  const { root } = useStyles();
   return (
     <Autocomplete
       id='search-box'
-      inputValue={props.value || ''}
-      className={props?.className || ''}
+      limitTags={10}
+      className={`${root} ${props?.className || ''}`}
       options={props.options}
       onChange={props.onChange}
       getOptionLabel={(option) => option.label}
+      PaperComponent={Paper}
       renderInput={(params: any) => (
         <Input {...params} label={props.label} variant='outlined' />
       )}
