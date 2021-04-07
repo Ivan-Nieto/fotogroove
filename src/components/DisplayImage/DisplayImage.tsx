fb42cddad876e27d31b7631a4d62c5f3ef39492b
Ivan <ivan@inieto.dev>
2021-04-06 18:44:50 -0600

    Added border radious to images

--- src/components/DisplayImage/DisplayImage.tsx
+++ src/components/DisplayImage/DisplayImage.tsx
@@ -3,22 +3,18 @@ import { useTheme, Theme, makeStyles } from '@material-ui/core/styles';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { useSpring, animated as a } from 'react-spring';
 
-const useStyles = (url: string) =>
-  makeStyles((theme: Theme) => ({
-    card: {
-      background: 'grey',
-      border: `1px solid ${theme.palette.grey[200]}`,
-      margin: 0,
-      padding: 0,
-      borderRadius: '3px',
-      backgroundImage: `url(${url})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center center',
-      boxShadow: `0px 10px 15px -5px ${theme.palette.grey[200]}`,
-      transition: 'box-shadow 0.5s',
-      willChange: 'transform',
-    },
-  }));
+const useStyles = makeStyles((theme: Theme) => ({
+  card: {
+    background: 'grey',
+    border: `1px solid ${theme.palette.grey[200]}`,
+    margin: 0,
+    padding: 0,
+    borderRadius: '7px',
+    boxShadow: `0px 10px 15px -5px ${theme.palette.grey[200]}`,
+    transition: 'box-shadow 0.5s',
+    willChange: 'transform',
+  },
+}));
 
 interface Image {
   thumbUrl: {
@@ -50,10 +46,10 @@ const DisplayImage = ({ size, image }: DisplayImageProps) => {
   const theme = useTheme();
   const [url, setUrl] = useState('');
   const [loaded, setLoaded] = useState(false);
-  const classes = useStyles(url)(theme);
+  const classes = useStyles(theme);
 
   const trans = (x: any, y: any, s: any) => `scale(${s})`;
-  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 1, tension: 210, friction: 20 } }));
+  const [props, setProps] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 1, tension: 210, friction: 20 } }));
 
   useEffect(() => {
     const portraitLarge = image?.thumbUrl?.portrait;
@@ -76,7 +72,7 @@ const DisplayImage = ({ size, image }: DisplayImageProps) => {
   }, [size, setUrl, image]);
 
   return (
-    <div>
+    <>
       <a.img
         alt={image?.description || ''}
         onLoad={() => setLoaded(true)}
@@ -84,14 +80,14 @@ const DisplayImage = ({ size, image }: DisplayImageProps) => {
         width={size === 'small' ? 300 : 680}
         height={size === 'small' ? 200 : 400}
         className={classes.card}
-        onMouseMove={() => set({ xys: [0, 0, 1.01] })}
-        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        onMouseMove={() => setProps({ xys: [0, 0, 1.01] })}
+        onMouseLeave={() => setProps({ xys: [0, 0, 1] })}
         // @ts-ignore
         style={{ transform: props?.xys?.interpolate(trans) }}
       />
 
       {!loaded && <Skeleton animation='wave' variant='rect' width={size === 'small' ? 300 : 680} height={size === 'small' ? 200 : 400} />}
-    </div>
+    </>
   );
 };
 
